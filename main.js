c9eb78074311cec087033a3e2939a84812a0aa91
Michael Tobechukwu Nwokike <76704861+Michael-Tobechukwu@users.noreply.github.com>
2022-06-21 21:35:15 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+const ZuriIntern = {
+    fullName: "Michael Tobechukwu Nwokike",
+    height: 5.10,
+    country: "Nigeria",
+    }
+
+var fullname = document.getElementById ('name');
+var height = document.getElementById ('height');
+var country = document.getElementById ('country');
+
+fullname.innerText = ZuriIntern.fullName;
+
+height.innerText = ZuriIntern.height;
+
+country.innerText = ZuriIntern.country;
